86ca81132ebe713d009168322d031f58258901a9
Yoganand Govind <yogieee@yoganands-air.home>
2024-02-26 17:41:32 +0000

    initial commit logo changes

--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -1,29 +1,28 @@
 "use client";
-import Image from "next/image";
+
 import Link from "next/link";
 import React, { useRef } from "react";
 import Mainlogo from "./Mainlogo";
 import { gsap } from "gsap";
 
-const Logo = () => {
-  const logoRef = useRef(null);
+export default function Logo() {
+  const logoRef = useRef<HTMLDivElement>(null);
 
-  const onLogoEnter = () => {
-    const currColor = logoRef.current.getAttribute("data-color");
-    const logoSvg = logoRef.current.firstChild.firstChild;
+  function onLogoEnter() {
+    const logoSvg = logoRef.current?.firstChild?.firstChild;
 
-    logoRef.current.setAttribute("data-color", "#a86485");
-    gsap.to(logoSvg, { fill: currColor, duration: 0.2, ease: "power1.out" });
-  };
-  const onLogoLeave = () => {
-    const logoSvg = logoRef.current.firstChild.firstChild;
+    gsap.to(logoSvg!, { fill: "#a86485", duration: 0.2, ease: "power1.out" });
+  }
 
-    gsap.to(logoSvg, {
+  function onLogoLeave() {
+    const logoSvg = logoRef.current?.firstChild?.firstChild;
+
+    gsap.to(logoSvg!, {
       fill: "#3a4454",
       duration: 0.2,
       ease: "power1.out",
     });
-  };
+  }
 
   return (
     <div>
@@ -40,6 +39,4 @@ const Logo = () => {
       </Link>
     </div>
   );
-};
-
-export default Logo;
+}
